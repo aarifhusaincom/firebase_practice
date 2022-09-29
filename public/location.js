7292da717b8e456329ad0dd08ce0d1bbcca4f89e
Aarif Husain <mail@aarifhusain.com>
2022-09-29 17:43:56 +0530

    121212

--- public/location.js
+++ public/location.js
@@ -1,13 +1,63 @@
-const successCa11back = (position) => {
+var successCa11back = (position) => {
     console.log(position);
-    };
-    
-    
-    const errorCa11back = (error) => {
+
+    var accuracy = position.coords.accuracy;
+    var altitude = position.coords.altitude;
+    var altitudeaccuracy = position.coords.altitudeaccuracy;
+    var heading = position.coords.heading;
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    var speed = position.coords.speed;
+
+
+
+
+async function insertLocation() {
+
+    var jsonData = {
+        "accuracy": accuracy,
+        "altitude": altitude,
+        "altitudeaccuracy": altitudeaccuracy,
+        "heading": heading,
+        "latitude": latitude,
+        "longitude": longitude,
+        "speed": speed
+
+    }
+    console.log("before", JSON.stringify(jsonData));
+
+    fetch("https://cerbosys.in:5900/location/insertLocation",
+        {
+            method: "POST",
+            body: jsonData,
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        })
+        .then((res) => {
+            console.log(res);
+            console.log("After", res.json);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+
+};
+
+    insertLocation();
+};
+
+
+
+
+
+
+
+
+
+var errorCa11back = (error) => {
     console.error(error);
-    };
+};
 
-
-    const watchld = navigator.geolocation.watchPosition(successCa11back, errorCa11back);
-    
-    navigator.geolocation.clearWatch(watchId);
+var watchld = navigator.geolocation.watchPosition(successCa11back, errorCa11back);
+navigator.geolocation.clearWatch(watchId);
