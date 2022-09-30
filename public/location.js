4255928ddf6edead379f4bdb1e954b181af15154
Aarif Husain <mail@aarifhusain.com>
2022-09-30 12:30:26 +0530

    fcbh

--- public/location.js
+++ public/location.js
@@ -1,63 +1,42 @@
 var successCa11back = (position) => {
-    console.log(position);
+  console.log(position);
 
-    var accuracy = position.coords.accuracy;
-    var altitude = position.coords.altitude;
-    var altitudeaccuracy = position.coords.altitudeaccuracy;
-    var heading = position.coords.heading;
-    var latitude = position.coords.latitude;
-    var longitude = position.coords.longitude;
-    var speed = position.coords.speed;
+  var accuracys = position.coords.accuracy;
+  var altitudes = position.coords.altitude;
+  var altitudeaccuracys = position.coords.altitudeaccuracy;
+  var headings = position.coords.heading;
+  var latitudes = position.coords.latitude;
+  var longitudes = position.coords.longitude;
+  var speeds = position.coords.speed;
 
+  async function insertLocation() {
+    axios
+      .post("https://cerbosys.in:5900/location/insertLocation", {
+        accuracy: accuracys,
+        altitude: altitudes,
+        altitudeaccuracy: altitudeaccuracys,
+        heading: headings,
+        latitude: latitudes,
+        longitude: longitudes,
+        speed: speeds,
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
 
-
-
-async function insertLocation() {
-
-    var jsonData = {
-        "accuracy": accuracy,
-        "altitude": altitude,
-        "altitudeaccuracy": altitudeaccuracy,
-        "heading": heading,
-        "latitude": latitude,
-        "longitude": longitude,
-        "speed": speed
-
-    }
-    console.log("before", JSON.stringify(jsonData));
-
-    fetch("https://cerbosys.in:5900/location/insertLocation",
-        {
-            method: "POST",
-            body: jsonData,
-            headers: {
-                "Content-type": "application/json; charset=UTF-8"
-            }
-        })
-        .then((res) => {
-            console.log(res);
-            console.log("After", res.json);
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-
+  insertLocation();
 };
-
-    insertLocation();
-};
-
-
-
-
-
-
-
-
 
 var errorCa11back = (error) => {
-    console.error(error);
+  console.error(error);
 };
 
-var watchld = navigator.geolocation.watchPosition(successCa11back, errorCa11back);
+var watchld = navigator.geolocation.watchPosition(
+  successCa11back,
+  errorCa11back
+);
 navigator.geolocation.clearWatch(watchId);
